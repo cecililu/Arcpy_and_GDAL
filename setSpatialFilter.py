fd2bbb2ca31ceda02d3f26b78cfae1f7986edbe0
cecil <cecilghimire@gmail.com>
2022-12-15 21:46:55 +0545

    find polygon for a point

--- setSpatialFilter.py
+++ setSpatialFilter.py
@@ -1,6 +1,6 @@
 from osgeo import gdal,ogr
 import sys,os
-
+from osgeo import osr
 try:
     shapefile = r'C:\Users\msi\Desktop\Arcpy and Gdal\Lalitpurshp.shp'
     driver = ogr.GetDriverByName("ESRI Shapefile")
@@ -13,14 +13,23 @@ try:
     else:
         print ('Opened ' ,shapefile)
         layer = dataSource.GetLayer()
+        # layer.SetAttributeFilter("TYPE = 'Gaunpalika'")
         featureCount = layer.GetFeatureCount()
-        print ("Number of features in"+ os.path.basename(shapefile),":  ",featureCount)
+        print ("Number of Local level Gaupalika  in Lalitpur"+ os.path.basename(shapefile),":  ",featureCount)
+        print("They are listed below")
         
-        layer.SetAttributeFilter("TYPE = 'Gaunpalika")
         i = 1
         for feature in layer:
-             print (i, feature.GetField("LOCAL"))
-             i = i + 1
+             print (i, feature.GetField("LOCAL"),"::",feature.GetField("TYPE"),)
+             geom = feature.GetGeometryRef()
+            #  print (feature.GetField("LOCAL"), "has its centroid in-->", geom.Centroid().ExportToWkt())
+        # filter by point    #  
         
+        kt = "POINT (85.3043 27.4977 )"
+        ply=ogr.CreateGeometryFromWkt(kt)
+        layer.SetSpatialFilter(ply)
+        for feature in layer: 
+             print ('Point lies in this Local level',feature.GetField("LOCAL"),',',feature.GetField("TYPE"))
+             
 except:
     sys.exit(1)
