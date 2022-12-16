6157ac0471818fc414c19851cf6f3f763410eaee
cecil <cecilghimire@gmail.com>
2022-12-16 14:43:33 +0545

    timer

---
+++ TimeElapsed.py
@@ -0,0 +1,9 @@
+import datetime
+import time
+start=time.time()
+a=1
+while(True):
+    current=time.time()
+    elapsed = datetime.timedelta(seconds=current-start)
+    print ('Time',elapsed)
+    a=a+1
